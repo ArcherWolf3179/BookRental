b1cee54524c9e706d595f449120266585d2f8930
Avyukta Dinesh <archerwolf3179@gmail.com>
2024-02-11 14:11:27 -0700

    It now shows all of the titles by Bill Bryson

--- mongo.py
+++ mongo.py
@@ -2,13 +2,21 @@ from pymongo import MongoClient
 
 URL = "mongodb://localhost:27017/book"
 
-def checkUrl(url):
-    try:
-        client = MongoClient(url)
-        db = client.get_database()
-        print("Connection successful!")
-        client.close()
-        return True
-    except Exception as e:
+
+try:
+    client = MongoClient(URL)
+    db = client.get_database()
+    print("Connection successful!")
+
+except Exception as e:
         print("Connection failed:", e)
-        return False
+    
+def read(collection,query):
+    result = collection.find(query)
+    for doc in result:
+        print(doc)
+
+mycollection = db["books"]
+q = {"authors" : "Bill Bryson"}
+
+read(mycollection,q)
