258caebef893aae6bc9c04458ce9d59765c3db75
Avyukta Dinesh <archerwolf3179@gmail.com>
2024-02-16 09:04:45 -0700

    I made it so that it gives the value we want and shows the title For example Title : A Short History of Nearly Everything

--- mongo.py
+++ mongo.py
@@ -1,22 +1,3 @@
-"""def create(collection): #Trying to add to the collection rentedBooks
-    try:
-        new_doc = frozenset({'_id': ObjectId('65bc50930a2f3c9a1827dd79'), 
-                'bookID': 21, 'title': 'A Short History of Nearly Everything',
-                'authors': 'Bill Bryson',
-                'average_rating': 4.21,
-                'isbn': '076790818X',
-                'isbn13': 9780767908184,
-                'language_code': 'eng',
-                '  num_pages': 544,
-                'ratings_count': 248558,
-                'text_reviews_count': 9396,
-                'publication_date': '9/14/2004',
-                'publisher': 'Broadway Books'})
-        insert_result = collection.insert_one({new_doc})
-        print(f"Inserted document ID: {insert_result.inserted_id}")
-    except Exception as e:
-         print(f"There was an error: {e}")"""
-
 from pymongo import MongoClient
 #from bson import ObjectId
 
@@ -35,7 +16,11 @@ def read(collection,query,specific_valuee):
         result = collection.find(query)
         for doc in result:
              specific_value = doc[specific_valuee]
-             print(f"Specific Value: {specific_value}")
+
+             if type(specific_valuee) == str:
+                
+                print(f"{specific_valuee.capitalize()} : {specific_value}")
+
     except Exception as e:
          print(f"There was an error: {e}")
 
@@ -46,7 +31,7 @@ try:
     rentedBooks = db["rentedBooks"]
     q = {"title" : "A Short History of Nearly Everything"}
 
-    read(allbooks,q,"bookID")
+    read(allbooks,q,"title")
 
     client.close()
 except Exception as e:
