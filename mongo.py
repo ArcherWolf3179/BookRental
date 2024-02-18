b19d18b576d22d5d25cc0730231435e4a7a26343
Avyukta Dinesh <archerwolf3179@gmail.com>
2024-02-18 14:52:12 -0700

    Deleted uneeded comment block

--- mongo.py
+++ mongo.py
@@ -1,22 +1,3 @@
-"""def create(collection): #Trying to add to the collection rentedBooks
-    try:
-        new_doc = frozenset({'_id': ObjectId('65bc50930a2f3c9a1827dd79'), 
-                'bookID': 21, 'title': 'A Short History of Nearly Everything',
-                'authors': 'Bill Bryson',
-                'average_rating': 4.21,
-                'isbn': '076790818X',
-                'isbn13': 9780767908184,
-                'language_code': 'eng',
-                '  num_pages': 544,
-                'ratings_count': 248558,
-                'text_reviews_count': 9396,
-                'publication_date': '9/14/2004',
-                'publisher': 'Broadway Books'})
-        insert_result = collection.insert_one({new_doc})
-        print(f"Inserted document ID: {insert_result.inserted_id}")
-    except Exception as e:
-         print(f"There was an error: {e}")"""
-
 from pymongo import MongoClient
 #from bson import ObjectId
 
