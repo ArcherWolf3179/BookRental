7c762cda7f0472b681144b1b63d9da3f2e064a74
Avyukta Dinesh <archerwolf3179@gmail.com>
2024-02-19 10:31:14 -0700

    So I made it so that you can rent a book now. You just enter in the book you want to rent and then it will add it to the rented books collection. If it is already rented then it will tell you that it was already rented

--- mongo.py
+++ mongo.py
@@ -24,18 +24,30 @@ def read(collection,query,specific_valuee):
     except Exception as e:
          print(f"There was an error: {e}")
 
-def rent_A_Book(allBooksCollection,rentedBooksCollection):
-    pass
+def rent_A_Book(rentedBooksCollection,newDoc):
+    try:
+
+        result = rentedBooksCollection.find(newDoc)
+
+        if result:
+            print("Book is already rented")
+        else:
+            insert_result = rentedBooksCollection.insert_one(newDoc)
+            print(f"Inserted doc ID: {insert_result}")
+    except Exception as e:
+        print(f"There was an error {e}")
 
 # bookID,title,authors,average_rating,isbn,isbn13,language_code,  num_pages,ratings_count,text_reviews_count,publication_date,publisher
 
 try:
     allbooks = db["books"]
     rentedBooks = db["rentedBooks"]
+    userDB = db["user"]
     q = {"title" : "A Short History of Nearly Everything"}
+    new_Docs = {"title" : "A Short History of Nearly Everything", "author" : "Bill Bryson"}
 
     read(allbooks,q,"title")
-
+    rent_A_Book(rentedBooks,new_Docs)
 
     client.close()
 except Exception as e:
