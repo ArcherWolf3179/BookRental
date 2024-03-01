2239cafe807bfcd96ec1e53b481063f9c5232b56
Avyukta Dinesh <archerwolf3179@gmail.com>
2024-03-01 15:10:34 -0700

    Deleted generate user ID function since we don't need it now

--- mongo.py
+++ mongo.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import random
 #from bson import ObjectId
 
 URL = "mongodb://localhost:27017/book"
@@ -11,31 +12,66 @@ try:
 except Exception as e:
     print("Connection failed:", e)
     
-def read(collection,query,specific_valuee):
+
+def read(collection, query, specific_valuee):
     try:
-        result = collection.find(query)
+        a = []
+        result = collection.find({'title':{'$regex':f'^{query}'}})
         for doc in result:
-             specific_value = doc[specific_valuee]
+            a.append(doc[specific_valuee])
 
-        if type(specific_valuee) == str:
-            print(f" {specific_valuee.capitalize()} : {specific_value}")
-
+        for ei in a:
+            if query in ei:
+                print(ei)
 
     except Exception as e:
-         print(f"There was an error: {e}")
+        print(f"There was an error: {e}")
 
-def rent_A_Book(rentedBooksCollection,newDoc):
+def rent_A_Book(allbooks,userDB,rentedBooks,newDoc,specificvalue):
     try:
-
-        result = rentedBooksCollection.find(newDoc)
+        result = rentedBooks.find({specificvalue : newDoc})
+        findUser = userDB.find({"ID" : 1})
+        findBook = allbooks.find({specificvalue : newDoc})
 
         if result:
             print("Book is already rented")
+
+        elif findBook:
+            print("We don't have your book")
+
         else:
-            insert_result = rentedBooksCollection.insert_one(newDoc)
-            print(f"Inserted doc ID: {insert_result}")
+            
+            if findUser:
+                insert_result = rentedBooks.insert_one({specificvalue : newDoc,"ID" : 1})
+                print(f"Inserted doc ID: {insert_result}")
+            else:
+                print("You need to sign up")
+
     except Exception as e:
         print(f"There was an error {e}")
+
+def return_Book(rentedbooksCollection,query):
+    try:
+        rentedbooksCollection.delete_one(query)
+    except Exception as e:
+        print(f"There was an error: {e}")
+
+def SignUp(userCollection,email,number):
+    try:
+        e = userCollection.find({"email":email}) 
+        n = userCollection.find({"number" : number})
+
+        if n == True or e == True:
+            print("You've already signed up here before with this email or number")
+        else:
+            userID = 1
+            userInfo = {"email":email,"number" : number, "ID" : userID}
+            userCollection.insert_one({"email":email,"number" : number, "ID" : userID})
+            print("Inserted user info")
+            del(userInfo,e,n,userID)
+
+    except Exception as e:
+        print(f"There was an error function SIgn up: {e}")
 
 # bookID,title,authors,average_rating,isbn,isbn13,language_code,  num_pages,ratings_count,text_reviews_count,publication_date,publisher
 
@@ -43,12 +79,9 @@ try:
     allbooks = db["books"]
     rentedBooks = db["rentedBooks"]
     userDB = db["user"]
-    q = {"title" : "A Short History of Nearly Everything"}
-    new_Docs = {"title" : "A Short History of Nearly Everything", "author" : "Bill Bryson"}
 
-    read(allbooks,q,"title")
-    rent_A_Book(rentedBooks,new_Docs)
+    #SignUp(userDB,"afkldafdsf@gmail.com","123-456-7890")
+    rent_A_Book(allbooks,userDB,rentedBooks,"A Short History of Nearly Everything","title")
 
-    client.close()
 except Exception as e:
-     print(f"There was an error {e}")
+    print(f"There was an error {e}")
