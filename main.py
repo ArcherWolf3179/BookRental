092d5787a91a4cc4231fb9108bdb9d1ba1b5218f
Avyukta Dinesh <archerwolf3179@gmail.com>
2024-03-05 16:12:22 -0700

    Made it connect to a html file

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from backend import mongo
-import flask
+from flask import Flask, redirect, url_for, render_template
+from logging import FileHandler, WARNING
+import os
 
-app = flask.Flask(__name__)
-@app.route("/")
+try:
+    app = Flask(__name__)
+    htmlfile = r"C:\Users\avyuk\OneDrive\Desktop\BookRental\templates\index.html"
+    @app.route("/")
 
-def home():
-    r = mongo.read(mongo.allbooks,"A Short","title")
-    return flask.render_template("index.html", context=r)
+    def home():
+        try:
+            r = mongo.read(mongo.allbooks,"A Short","title")
+            return render_template('index.html')
+        
+        except FileNotFoundError as e:
+            print(f"The file wasn't found more info can be found here {e}")
 
-@app.route("/admin")
+        except Exception as e:
+            print(f"There was an error: {e}")
 
-def admin():
-    return flask.redirect(flask.url_for("home"))
+    @app.route("/admin")
 
-if __name__ == "__main__":
-    app.run()
+    def admin():
+        return redirect(url_for("home"))
+
+    if __name__ == "__main__":
+        app.run()
+
+except TypeError as e:
+    print(f"There was a type error please check the {e} for the correct type")
+
+except TimeoutError:
+    print("Something timedout")
+
+except Exception as e:
+    print(f"There was an error {e}")
